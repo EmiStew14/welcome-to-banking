18fac2eaa2da49b55c3b1cefb25eb30b177b2156
Emily Stewart <stewartemilym2014@gmail.com>
2021-05-01 14:58:01 -0700

    added column info to category and product

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,15 +6,32 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [ Product]
+  }).then((categories) => {
+    res.json(categories)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+const params = req.params.id;
+
+Category.findOne({
+  where: {id:params},
+  include: [ Product]
+}).then((categories) => {
+  res.json(categories)
+})
+})
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.params.category_name,
+    include: [Product]
+  })
 });
 
 router.put('/:id', (req, res) => {
